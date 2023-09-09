6db33bd905df52355bfa4d4fb9a5b4562b0f4f58
devjoshua22 <113951236+devjoshua22@users.noreply.github.com>
2023-09-09 14:28:17 +0100

    Add files via upload

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,3 @@
-const year = document.getElementById('year')
-const month = document.getElementById('month')
-const day = document.getElementById('day')
-const hour = document.getElementById('hour')
-const minute = document.getElementById('minute')
-const second = document.getElementById('second')
 const milliSecond = document.getElementById('millisecond')
 const currentDay = document.getElementById('current-day-of-the-week');
 const currentTime = document.getElementById('current-complete-time');
@@ -18,13 +12,9 @@ function currentDayDisplay(){
 
 function currentTimeDisplay(){
     const date = new Date();
-    year.innerText = date.getFullYear()
-     month.innerText = String(date.getMonth() + 1).padStart(2, '0'); // Month is zero-based, so add 1.
-     day.innerText = String(date.getDate()).padStart(2, '0');
-    hour.innerText = String(date.getHours()).padStart(2, '0');
-     minute.innerText = String(date.getMinutes()).padStart(2, '0');
-     second.innerText = String(date.getSeconds()).padStart(2, '0');
-     milliSecond.innerText = String(date.getMilliseconds()).padStart(3, '0');
+     const utcTime = date.getTime()
+  
+     milliSecond.innerText = utcTime
 }
 currentDayDisplay();
 currentTimeDisplay();
